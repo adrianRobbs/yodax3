950f0fcc5c8221606bca586c8428c1ff28878e3b
adrianRobbs <clipmakerstudios@gmail.com>
2022-04-18 09:53:59 +0000

    Add SEO tags

--- src/App.tsx
+++ src/App.tsx
@@ -9,41 +9,49 @@ import {
   Overlay,
   BikeOverlay,
 } from "./components";
+import { Helmet } from "react-helmet-async";
 
 function App() {
   return (
-    <Box
-      display="grid"
-      gridTemplateColumns="repeat(12, 1fr)"
-      gridTemplateRows="1fr"
-      sx={{
-        background:
-          "radial-gradient(700px 700px at 50px bottom, #F81D79 -50%, #181515)",
-      }}
-    >
+    <>
+      <Helmet>
+        <title>YodaX3 | Ride the streets</title>
+        <meta name="description" content="Take the streets with speed" />
+        <link rel="canonical" href="/" />
+      </Helmet>
       <Box
+        display="grid"
+        gridTemplateColumns="repeat(12, 1fr)"
+        gridTemplateRows="1fr"
         sx={{
-          height: "100vh",
-          maxWidth: "1280px",
-          width: "100%",
-          mx: "auto",
-          gridColumn: "1/-1",
-          gridRow: "1/-1",
-          display: "grid",
-          gridTemplateColumns: "repeat(12,1fr)",
-          gridTemplateRows: "66px 2fr 1.2fr",
+          background:
+            "radial-gradient(700px 700px at 50px bottom, #F81D79 -50%, #181515)",
         }}
       >
-        <Navigation />
-        <MainContent />
-        <LeftBottom1 />
-        <LeftBottom2 />
-        <RightTop />
-        <RightBottom />
+        <Box
+          sx={{
+            height: "100vh",
+            maxWidth: "1280px",
+            width: "100%",
+            mx: "auto",
+            gridColumn: "1/-1",
+            gridRow: "1/-1",
+            display: "grid",
+            gridTemplateColumns: "repeat(12,1fr)",
+            gridTemplateRows: "66px 2fr 1.2fr",
+          }}
+        >
+          <Navigation />
+          <MainContent />
+          <LeftBottom1 />
+          <LeftBottom2 />
+          <RightTop />
+          <RightBottom />
+        </Box>
+        <Overlay />
+        <BikeOverlay />
       </Box>
-      <Overlay />
-      <BikeOverlay />
-    </Box>
+    </>
   );
 }
 
