070e57d080fea88f50231686624b24c4cc76f0b0
adrianRobbs <clipmakerstudios@gmail.com>
2022-04-18 10:00:06 +0000

    Fix Helmet integration
    Add Crawls and Index
    
    Add Crawls and Index

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import ThemeContextProvider from "./context/ThemeContextProvider";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeContextProvider>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </ThemeContextProvider>
   </React.StrictMode>
 );
